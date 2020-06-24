46ab94902019274bd0d3f817cab27671a3e52b2a
Suyash Shukla <suyashku@gmail.com>
2020-06-24 18:51:45 +0530

    Combining Reducers

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody,
     CardTitle, Breadcrumb, BreadcrumbItem, Button,  Modal, ModalHeader, ModalBody,
-    Form, FormGroup, Input, Label, Col, Row } from 'reactstrap';
+     Label, Col, Row } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 
