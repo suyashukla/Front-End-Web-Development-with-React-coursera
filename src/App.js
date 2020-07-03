025134fea39106dc5863c4c71a944bfd79d17f4a
Suyash Shukla <suyashku@gmail.com>
2020-07-03 21:08:53 +0530

    Assignment 4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React,{Component} from 'react';
-import logo from './logo.svg';
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
